c970140b9443e0b660a571098f4020d28ffb749e
Sivanand <siva20078@gmail.com>
2012-11-24 13:38:52 +0530

    Part1

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :article_id, :body, :email, :name
+
+  belongs_to :article
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogCancanDevise::Application.config.secret_token = 'c1386eed2c0ca02e4bc9f49464687b2bb8e623998de236c8cf5ed7553e17c596270d3610e04186b4b4e40146dcdce4ca8696f96472a94262ed37fd1883aadd2d'
